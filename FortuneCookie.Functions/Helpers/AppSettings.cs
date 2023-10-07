5df04f2703354aceb67990514d08236e17f4c47c
Taras Iskiv <taras.iskiv@gmail.com>
2023-10-07 10:15:47 +0200

    Removed not more needed file

--- FortuneCookie.Functions/Helpers/AppSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace FortuneCookie.Functions.Helpers;
-
-public class AppSettings
-{
-    public string Database { get; set; } = default!;
-    public string TelegramBotKey { get; set; } = default!;
-}
